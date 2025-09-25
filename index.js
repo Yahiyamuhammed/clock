16875aebc467fdb8d21bd186668366e6ae2389b5
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-25 10:32:17 +0530

    feat: some changes

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const hourEl= document.getElementById("hour")
-const minuteEl= document.getElementById("minutes")
-const secondEl= document.getElementById("second")
-const ampmEl= document.getElementById("ampm")
-function updateClock()
-{
-    let h= new Date().getHours();
-    let m= new Date().getMinutes();
-    let s= new Date().getSeconds();
-    let ampm="AM"
-    if(h>12)
-    {
-        h=h-12
-        ampm="pm";
+function displayTime(){
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+    var session = document.getElementById('session');
+
+    if(hrs >= 12){
+        session.innerHTML = 'PM';
+    }else{
+        session.innerHTML = 'AM';
     }
-    h=h>11
-    0?"0"+h:h;
-    m=m>10?"0"+m:m;
-    s=s>10?"0"+s:s;
-    hourEl.innerText=h;
-    minuteEl.innerText=m;
-    secondEl.innerText=s;
-    ampmEl.innerText=ampm;
-    setTimeout(() => 
-    {
-        updateClock()
-    }, 1000);
+
+    if(hrs > 12){
+        hrs = hrs - 12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
 }
-updateClock();
+setInterval(displayTime, 10);
